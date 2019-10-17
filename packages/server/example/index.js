1ab265e0030563f71d9015b1bf38c986668b1c82
Sampson <dujianwxfdt@live.cn>
2019-10-17 20:01:14 +0800

    bug

--- packages/server/example/index.js
+++ packages/server/example/index.js
@@ -2,14 +2,14 @@ const PPTParseSDK = require('../src/index')
 let sdk = new PPTParseSDK()
 
 const path = require('path')
-const fs = require("fs")
+const fs = require('fs')
 
-const exmaplePath = path.join(__dirname,'../../../example')
+const exmaplePath = path.join(__dirname, '../../../example')
 
-sdk.parsePPT(path.join(exmaplePath,'test7.pptx'),path.join(exmaplePath,'pptOutput1')).then(()=>{
+sdk.parsePPT(path.join(exmaplePath, 'test7.pptx'), path.join(exmaplePath, 'pptOutput1')).then(() => {
     let json = JSON.stringify(sdk.json)
-    fs.writeFileSync(path.join(exmaplePath,'pptOutput1/output.json'))
-    let links = Object.keys(sdk.json.fonts).map(c=>{
+    fs.writeFileSync(path.join(exmaplePath, 'pptOutput1/output.json'), json)
+    let links = Object.keys(sdk.json.fonts).map(c => {
         return `<link href="https://fonts.googleapis.com/css?family=${c}&display=swap" rel="stylesheet">`
-    }).join("\n")
+    }).join('\n')
 })
